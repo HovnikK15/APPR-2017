7fc1ee676619346428c8ff04404e2a7e7d1cdbec
Klemen Hovnik <klemen.hovnik@gmail.com>
2017-05-08 10:35:49 +0200

    ok

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -13,15 +13,29 @@ sl <- locale("sl", decimal_mark = ".")
 
 View(tabela)
 
-# Funkcija, ki uvozi podatke iz datoteke Meddrzavne_selitve.csv
+
+uvozihtml <- function() {
+
+html <- file("podatki/html1.html") %>% readLines()
+podatkiHTML <- grep("var dataValues", html, value = TRUE) %>%
+  strapplyc('var dataValues="([^"]+)"') %>% .[[1]] %>%
+  strsplit("|", fixed=TRUE) %>% unlist() %>%
+  matrix(ncol=6, byrow=TRUE)
+return(html)
+}
+html1 <- uvozihtml()
+
+
+
+
+# Funkcija, ki uvozi podatke iz csv datotek v mapi "podatki"
 
 library(readr)
 library(tidyr)
 library(gsubfn)
 library(dplyr)
-
-uvozi <- function() {
-  tab <- read_csv2(file="podatki/05N1004Ss.csv",
+uvozi1 <- function() {
+  tab <- read_csv2(file="podatki/tabela1.csv",
                    col_names = c("Vrsta_migrantov", "Starostna_skupina", "Leto", "Spol", "Stevilo"),
                    locale=locale(encoding="Windows-1250"),skip = 4,  n_max = 1865) 
   tab <- tab %>% fill(1:4) %>% drop_na(Stevilo) %>% filter(Starostna_skupina != "SKUPAJ",
@@ -30,7 +44,38 @@ uvozi <- function() {
   tab$Starostna_skupina <- NULL
   return(tab)
 }
-tabela <- uvozi()
+tabela1 <- uvozi1()
+
+uvozi2 <- function() {
+  tab2 <- read_csv2(file="podatki/tabela2.csv",
+                   col_names = c("Vrsta_migrantov", "Drzava_drzavljanstva", "Leto", "Spol", "Stevilo"),
+                   locale=locale(encoding="Windows-1250"),skip = 5,  n_max = 3336) 
+  tab2 <- tab2 %>% fill(1:4) %>% drop_na(Stevilo) %>% filter(Drzava_drzavljanstva != "Država državljanstva - SKUPAJ",
+                                                             Drzava_drzavljanstva != "EVROPA")
+  return(tab2) 
+}
+tabela2 <- uvozi2()
+
+uvozi3 <- function() {
+  tab3 <- read_csv2(file="podatki/tabela3.csv",
+                   col_names = c("Vrsta_migrantov", "Starostna_skupina", "Leto", "Spol","Drzavljanstvo", "Stevilo"),
+                   locale=locale(encoding="Windows-1250"),skip = 7,  n_max = 4525) 
+  tab3 <- tab3 %>% fill(1:5) %>% drop_na(Stevilo) %>% filter(Starostna_skupina != "Starostne skupine - SKUPAJ")
+  tab3$leta_min <- tab3$Starostna_skupina %>% strapplyc("(^[0-9]+)") %>% unlist() %>% parse_number()
+  tab3$Starostna_skupina <- NULL
+
+  return(tab3)
+}
+tabela3 <- uvozi3()
+
+uvozi4 <- function() {
+  tab4 <- read_csv2(file="podatki/tabela4.csv",
+                    col_names = c("Vrsta_migrantov", "Drzavljanstvo", "Leto", "Regija","Stevilo"),
+                    locale=locale(encoding="Windows-1250"),skip = 5,  n_max = 840) 
+  tab4 <- tab4 %>% fill(1:3) %>% drop_na(Stevilo)
+  return(tab4)
+}
+tabela4 <- uvozi4()
 
 
 
@@ -46,3 +91,4 @@ druzine <- uvozi.druzine(levels(obcine$obcina))
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+
